2e341bc8d84bcb694329216a69ecbfbabe479fa0
DmitryMikheiko <mikheiko.dmitry@gmail.com>
2020-02-27 17:19:16 +0300

    algorithm changed, unstable, need to fix

---
+++ Config.h
@@ -0,0 +1,9 @@
+#ifndef _CONFIG_
+#define _CONFIG_
+
+#define TestBoardRemap
+#define CRCEN
+#define CharWaitDelay       200     // *100us 200*100us=20ms
+#define RX_BUFFER_SIZE      45      // uart input buffer size 32..x bytes
+
+#endif
